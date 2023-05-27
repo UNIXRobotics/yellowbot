982ba3eb119f308f0c08c9db8484ab38cce5ea5b
Evan <evanrayweigel@outlook.com>
2023-05-27 15:32:57 -0500

    make an actual rust vex-rt project

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # yellowbot
-the code for our vex v5 2023-2024
-Woo Hoo
+
+The code for our Vex v5 Robot 2023-2024
+
+# Getting started with Rust on V5
+
+This project was bootstrapped with the [Rust on V5 Template](https://gitlab.com/qvex/vex-rt-template).
+
+## Available Scripts
+
+In the project directory you can run:
+
+### `cargo check`
+
+Checks the project for warnings and errors. This is faster than a full build.
+
+### `cargo build`
+
+Compiles and builds the project.
+
+### `cargo run`
+
+Does the same thing as `cargo build` but also launches `prosv5 ut` and uploads to the robot.
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,70 @@
 #![no_std]
 #![no_main]
 
-use vex_rt::prelude::*;
+use core::time::Duration;
 
-struct PissBot;
+use vex_rt::{prelude::*, select};
 
-impl Robot for PissBot {
-    fn new(_peripherals: Peripherals) -> Self {
-        println!("Hello, pissbot");
-        Self
+mod drive;
+
+struct MyRobot {
+    drive: Mutex<drive::Drive>,
+    controller: Controller,
+}
+
+impl Robot for MyRobot {
+    fn new(peripherals: Peripherals) -> Self {
+        Self {
+            drive: Mutex::new(drive::Drive {
+                left_drive: peripherals
+                    .port01
+                    .into_motor(Gearset::EighteenToOne, EncoderUnits::Degrees, false)
+                    .unwrap(),
+                right_drive: peripherals
+                    .port02
+                    .into_motor(Gearset::EighteenToOne, EncoderUnits::Degrees, true)
+                    .unwrap(),
+            }),
+            controller: peripherals.master_controller,
+        }
+    }
+
+    fn initialize(&mut self, _ctx: Context) {
+        // Do any extra initialization here.
+    }
+
+    fn autonomous(&mut self, _ctx: Context) {
+        println!("autonomous");
+        // Write your autonomous routine here.
+    }
+
+    fn opcontrol(&mut self, ctx: Context) {
+        println!("opcontrol");
+
+        // This loop construct makes sure the drive is updated every 10
+        // milliseconds.
+        let mut l = Loop::new(Duration::from_millis(10));
+        loop {
+            // Update the motors.
+            self.drive.lock().run(
+                self.controller.left_stick.get_y().unwrap(),
+                self.controller.right_stick.get_y().unwrap(),
+            );
+
+            select! {
+                // If the driver control period is done, break out of the loop.
+                _ = ctx.done() => break,
+
+                // Otherwise, when it's time for the next loop cycle, continue.
+                _ = l.select() => continue,
+            }
+        }
+    }
+
+    fn disabled(&mut self, _ctx: Context) {
+        println!("disabled");
+        // This runs when the robot is in disabled mode.
     }
 }
 
-entry!(PissBot);
+entry!(MyRobot);
